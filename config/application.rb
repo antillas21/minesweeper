762ec1dc857547e90fa91b2abf94961f2ee276d1
Antonio Antillon <antillas21@gmail.com>
2024-01-13 14:17:14 -0800

    FactoryBot should not be in application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,5 @@ module Minesweeper
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.factory_bot.definition_file_paths = ["spec/factories"]
   end
 end
